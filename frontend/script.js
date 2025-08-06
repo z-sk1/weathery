db30761db5f4421a3dc0a4c4d0bc57b7565b2fdc
Ziad Skafi <ziad.skafi12@gmail.com>
2025-08-06 16:40:55 +0400

    added copy button

--- frontend/script.js
+++ frontend/script.js
@@ -70,7 +70,36 @@ function updateDisplay(data) {
     <p>💧 Humidity: ${data.humidity}%</p>
     <p>🌧️ Precipitation: ${data.precipitation} mm</p>
     <p>💨 Wind Speed: ${data.windSpeed} km/h</p>
-    <p>🌤️ Condition: ${description || "Unknown"}</p>`;
+    <p>🌤️ Condition: ${description || "Unknown"}</p>
+    <button onclick = "copyData()" id = "copyBtn">Copy</button>`;
+}
+
+function copyData() {
+    const resultDiv = document.getElementById("result");
+    const copyBtn = document.getElementById("copyBtn");
+    const city = document.getElementById("cityInput").value;
+    let textToCopy = "";
+
+    textToCopy += city;
+
+    for (let node of resultDiv.childNodes) {
+        if (node.nodeType === Node.ELEMENT_NODE && node.tagName === "BUTTON") {
+            continue;
+        }
+
+        if (node.nodeType === Node.TEXT_NODE || node.nodeType === Node.ELEMENT_NODE) {
+            textToCopy += node.textContent;
+        }
+    }
+
+    navigator.clipboard.writeText(textToCopy.trim())
+        .then(() => {
+            copyBtn.innerText = "Copied!";
+            setTimeout(() => {copyBtn.innerText = "Copy"}, 3000);
+        })
+        .catch(err => {
+            alert("Failed to copy! Error: ", err);
+        });
 }
 
 async function getWeather() {
